2d0993381f9b72b5fc55d5e0d9ebfe8d43da16de
spencerperley <spencerp11338@gmail.com>
2021-11-04 17:52:12 -0700

    worked on some stuf

--- src/Lab 4/main.ino
+++ src/Lab 4/main.ino
@@ -1,9 +1,17 @@
+/*
+* EE151 - Janine Darto
+* Spencer Perley
+* Lab 4
+*
+* Demonstration Video https://drive.google.com/file/d/1DBs1iw4zZwAYG57cEL6XtJmWq9pe75NW/view?usp=sharing
+*/
+
 #include "DHT.h";
 #include <LiquidCrystal.h>;
 
 // Import the Liquid Crystal library
 DHT dht(10, DHT11);
-LiquidCrystal lcd(12, 11, 5, 4, 13, 9);
+LiquidCrystal lcd(12, 11, 5, 4, 9, 13);
 
 int setTemp = 70;
 bool lcdAterantor = false;
@@ -17,9 +25,9 @@ int previousTime = 0;
 int currentTime = 0;
 
 // constants for the pins for the fan heater and cooler
-const int fanPin = 6;
-const int heaterPin = 8;
-const int coolerPin = 7;
+const int FAN_PIN = 6;
+const int HEATER_PIN = 8;
+const int COOLER_PIN = 7;
 
 void setup()
 {
@@ -28,13 +36,13 @@ void setup()
 	lcd.begin(16, 2);
 	dht.begin();
 	// Set the pin modes for the fan, heater and cooler
-	pinMode(fanPin, OUTPUT);
-	pinMode(heaterPin, OUTPUT);
-	pinMode(coolerPin, OUTPUT);
+	pinMode(FAN_PIN, OUTPUT);
+	pinMode(HEATER_PIN, OUTPUT);
+	pinMode(COOLER_PIN, OUTPUT);
 	// Set the fan, heater and cooler to off
-	digitalWrite(fanPin, LOW);
-	digitalWrite(heaterPin, LOW);
-	digitalWrite(coolerPin, LOW);
+	digitalWrite(FAN_PIN, LOW);
+	digitalWrite(HEATER_PIN, LOW);
+	digitalWrite(COOLER_PIN, LOW);
 
 	pinMode(2, INPUT_PULLUP);
 	pinMode(3, INPUT_PULLUP);
@@ -43,16 +51,8 @@ void setup()
 	attachInterrupt(digitalPinToInterrupt(3), decreaseTemp, FALLING);
 }
 
-/*
-███╗   ███╗ █████╗ ██╗███╗   ██╗    ██╗      ██████╗  ██████╗ ██████╗ 
-████╗ ████║██╔══██╗██║████╗  ██║    ██║     ██╔═══██╗██╔═══██╗██╔══██╗
-██╔████╔██║███████║██║██╔██╗ ██║    ██║     ██║   ██║██║   ██║██████╔╝
-██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║     ██║   ██║██║   ██║██╔═══╝ 
-██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ███████╗╚██████╔╝╚██████╔╝██║     
-╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝                                                                                                                                              
-*/
 
-/*______________________________________________________________________________________________________________________________*/
+
 void loop()
 {
 	runLoopTime();
@@ -63,7 +63,8 @@ void loop()
 	if (lcdAterantor)
 	{
 		displaySetTemp();
-		// every loop increase the delayTime by the loopTime until it reaches 5000ms then reset delayTime to 0
+		// every loop increase the delayTime by the loopTime until 
+		//it reaches 5000ms then reset delayTime to 0
 		delayTime += loopTime;
 		if (delayTime >= 5000)
 		{
@@ -76,7 +77,8 @@ void loop()
 	else
 	{
 		displayTemp();
-		// every loop increase the delayTime by the loopTime until it reaches 5000ms then reset delayTime to 0
+		// every loop increase the delayTime by the loopTime until it reaches
+		// 5000ms then reset delayTime to 0
 		delayTime += loopTime;
 		if (delayTime >= 5000)
 		{
@@ -89,42 +91,35 @@ void loop()
 	// if the temperature is above the setTemp turn on the fan
 	if (temp > setTemp)
 	{
-		digitalWrite(fanPin, HIGH);
+		digitalWrite(FAN_PIN, HIGH);
 	}
 	else
 	{
-		digitalWrite(fanPin, LOW);
+		digitalWrite(FAN_PIN, LOW);
 	}
 	// if the temperature is below the setTemp turn on the heater
 	if (temp < setTemp)
 	{
-		digitalWrite(heaterPin, HIGH);
+		digitalWrite(HEATER_PIN, HIGH);
 	}
 	else
 	{
-		digitalWrite(heaterPin, LOW);
+		digitalWrite(HEATER_PIN, LOW);
 	}
 
-	// if the temperature is 5 above the set temp or the setTemp is two above and the humidity is above 80% turn on the cooler
+	// if the temperature is 5 above the set temp or the setTemp is two above
+	// and the humidity is above 80% turn on the cooler
 	if (temp > setTemp + 5 || (setTemp > 2 && humidity > 80))
 	{
-		digitalWrite(coolerPin, HIGH);
+		digitalWrite(COOLER_PIN, HIGH);
 	}
 	else
 	{
-		digitalWrite(coolerPin, LOW);
+		digitalWrite(COOLER_PIN, LOW);
 	}
 }
-/*______________________________________________________________________________________________________________________________*/
 
-/*
-███████╗██╗   ██╗███╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗███████╗
-██╔════╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝
-█████╗  ██║   ██║██╔██╗ ██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████╗
-██╔══╝  ██║   ██║██║╚██╗██║██║        ██║   ██║██║   ██║██║╚██╗██║╚════██║
-██║     ╚██████╔╝██║ ╚████║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║███████║
-╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝                                                                      
-*/
+
 
 // a function that increases the setTemp by 1
 void increaseTemp()
@@ -167,7 +162,7 @@ void updateTemp()
 // a function to display the setTemp
 void displaySetTemp()
 {
-	// update the LCD screen 
+	// update the LCD screen
 	lcd.setCursor(0, 0);
 	lcd.print("Set Temp: ");
 	lcd.print(setTemp);
